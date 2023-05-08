5d9a45f2e9056c076812935ed421c48bd1d65afd
Pietro_Croce <pcroce91@gmail.com>
2023-05-08 17:51:47 +0200

    Fatto esercizio Reading and writing file

---
+++ Esercizio reading and writing file/Exercises.java
@@ -0,0 +1,126 @@
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Exercises {
+
+    private static final Path MY_PATH = Paths.get("file.txt");
+    private static final Path PATH_IN_A_FOLDER = Paths.get("mydirectory/file.txt");
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+        exercise5();
+    }
+
+    // Use the MY_PATH constant
+
+    /**
+     * 1:
+     * Write a method that writes myString to a file
+     * writes to a file
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+
+        String myString = """
+                The quick brown fox jumps over the lazy dog
+                """;
+        StringBuilder file = new StringBuilder();
+        file.append(myString);
+        try {
+            Files.createFile(MY_PATH);
+            Files.writeString(Path.of(MY_PATH.toUri()),file.toString());
+            // Your code here
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+           System.exit(0);
+        }
+    }
+
+    /**
+     * 2:
+     * Write a method that reads the file from exercise 1
+     * then prints it out
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        Path pathfile = Path.of("file.txt");
+
+        try {
+            // Your code here
+            System.out.println(Files.readString(pathfile));
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+
+
+    }
+
+    /**
+     * 3:
+     * Write a method that reads a file and print the number of lines in the file
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        // Write code here to read the file and return the number of lines "\n", string.split
+
+        Path pathfile = Path.of("file.txt");
+        try {
+            // Your code here
+            System.out.println(Arrays.stream(Files.readString(pathfile).split("\n")).count());
+
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+
+    }
+    /**
+     * 4:
+     * Write a method that reads a file and returns the number of words in the file
+     *
+     * Then deletes the previous file with Files.delete() use inside the try block
+     *
+     */
+    private static void exercise4() {
+        System.out.println("\nExercise 4: ");
+        try {
+            // Your code here
+            System.out.println(Arrays.stream(Files.readString(Path.of("file.txt")).split(" ")).count());
+            Files.delete(MY_PATH);
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * 5:
+     * Using the `myDirectoryPath` and Files.createDirectory, fix the code below
+     */
+    private static void exercise5() {
+        System.out.println("\nExercise 5: ");
+        Path myDirectoryPath = Path.of("mydirectory");
+
+        try {
+            // Your code
+            Files.createDirectory(myDirectoryPath);
+            Files.writeString(PATH_IN_A_FOLDER, "Why am I in a folder?");
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
